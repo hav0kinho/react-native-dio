dbf5ef29eff2fd4f96a078771c23272735eb6dea
Ruallyson Felype <ruallysonfelype@gmail.com>
2022-05-18 17:30:04 -0300

    Projeto Finalizado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet, SafeAreaView, StatusBar} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Pressable,
+  Linking,
+} from 'react-native';
 
 const githubColor = '#010409';
 const githubFontColor = '#C9D1D9';
 const githubDarkFontColor = '#4F565E';
-const githubImageProfile = 'https://avatars.githubusercontent.com/u/68474584?s=400&u=1882d6f66cff376323c0e1e971fb08b5cc2cee57&v=4';
-
+const githubImageProfile =
+  'https://avatars.githubusercontent.com/u/68474584?s=400&u=1882d6f66cff376323c0e1e971fb08b5cc2cee57&v=4';
+const urlToMyGithub = 'https://github.com/hav0kinho';
 
 const App = () => {
+  const handlePressGoToGithub = async () => {
+    const res = await Linking.canOpenURL(urlToMyGithub);
+    if (res) {
+      await Linking.openURL(urlToMyGithub);
+    }
+  };
+
   return (
     <SafeAreaView style={style.container}>
       <StatusBar backgroundColor={githubColor} barStyle="light-content" />
@@ -17,11 +34,32 @@ const App = () => {
           style={style.avatar}
           source={{uri: githubImageProfile}}
         />
-        <Text style={[style.defaultText, style.name]}>Ruallyson Felype</Text>
-        <Text style={[style.defaultText, style.nickname]}>hav0kinho</Text>
-        <Text style={[style.defaultText, style.description]}>
-          Estudante de Licenciatura em Computação | Programador Web | Front-end com conhecimentos em Back-end | Amante de Astronomia | Editor de Vídeos | Procurando Estágio
+        <Text
+          accessibilityLabel="Nome: Ruallyson Felype"
+          style={[style.defaultText, style.name]}>
+          Ruallyson Felype
         </Text>
+        <Text
+          accessibilityLabel="Nickname: hav0kinho"
+          style={[style.defaultText, style.nickname]}>
+          hav0kinho
+        </Text>
+        <Text
+          accessibilityLabel="Descrição: Estudante de Licenciatura em Computação | Programador Web | Front-end
+          com conhecimentos em Back-end | Amante de Astronomia | Editor de
+          Vídeos | Procurando Estágio"
+          style={[style.defaultText, style.description]}>
+          Estudante de Licenciatura em Computação | Programador Web | Front-end
+          com conhecimentos em Back-end | Amante de Astronomia | Editor de
+          Vídeos | Procurando Estágio
+        </Text>
+        <Pressable onPress={handlePressGoToGithub}>
+          <View style={style.button}>
+            <Text style={[style.defaultText, style.textButton]}>
+              Open in Gitgub
+            </Text>
+          </View>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
@@ -61,5 +99,15 @@ const style = StyleSheet.create({
   description: {
     fontWeight: 'bold',
     fontSize: 14,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: githubDarkFontColor,
+    borderRadius: 10,
+    padding: 20,
+  },
+  textButton: {
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
